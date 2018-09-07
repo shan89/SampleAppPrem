d4e3586569bc7f5f400b46721ae59a501bd00d98
Prem <premkumar.k@technoduce.com>
2018-09-07 16:16:17 +0530

    Initial Commit

---
+++ SampleAppPremGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleAppPremGit
+//
+//  Created by Sakthivel R on 07/09/18.
+//  Copyright © 2018 Sakthivel R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
